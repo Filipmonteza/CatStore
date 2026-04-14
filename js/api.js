cdd5eb12125b32203051c4186b13b7f7fe1b6947
Filip <Filipnordic@gmail.com>
2026-04-14 13:45:07 +0200

    first Init

---
+++ js/api.js
@@ -0,0 +1,7 @@
+async function fetchCats() {
+    try{
+        const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
+        const data = await response.json();
+        return data;
+    }
+}
