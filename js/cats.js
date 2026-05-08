e79e1ebf8d4ae13b8a09fd11fb6630aa365510f6
Filip <Filipnordic@gmail.com>
2026-05-08 10:14:11 +0200

    Updated: ReadMe, and fixed some bugs

--- js/cats.js
+++ js/cats.js
@@ -56,12 +56,13 @@ function renderCats() {
         loading="lazy"
         onerror="this.onerror=null; this.src='${fallbackImage}';"
       >
-      <button onclick='addToCart(${JSON.stringify(cat)})'>
-        Lägg i kundvagn
-      </button>
+      <button class="add-to-cart-btn">Lägg i kundvagn</button>
     `;
-
-    catList.appendChild(catCard);
+    
+      const btn = catCard.querySelector(".add-to-cart-btn");
+      btn.addEventListener("click", () => addToCart(cat));
+      
+      catList.appendChild(catCard);
   });
 
   const totalPages = Math.ceil(filteredCats.length / itemsPerPage);
